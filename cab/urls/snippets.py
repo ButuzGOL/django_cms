3beead0a326c08f083c0e43c7ad1019223f1667d
ButuzGOL <ButuzGOL@gmail.com>
2010-04-03 23:23:53 +0300

    chapter 10

--- cab/urls/snippets.py
+++ cab/urls/snippets.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list, object_detail
 from cab.models import Snippet
+from cab.views.snippets import add_snippet, edit_snippet
 
 snippet_info = { 'queryset': Snippet.objects.all() }
 
@@ -9,4 +10,6 @@ urlpatterns = patterns('',
         name='cab_snippet_list'),
     url(r'^(?P<object_id>\d+)/$', object_detail, snippet_info,
         name='cab_snippet_detail'),
+    url(r'^add/$', add_snippet, name='cab_snippet_add'),
+    url(r'^edit/(?P<snippet_id>\d+)/$', edit_snippet, name='cab_snippet_edit'),
 )
